b33cc5c5eeb8cc7da91453003c5994ae110ef863
Sahil Kumar <sahilkumar012004@gmail.com>
2024-02-24 04:18:07 +0530

    Added files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+//Audio
+const overaudio = new Audio("gameover.wav")
+const s2 = new Audio("s2.wav")
+
+
+
+
+
+
+let simonsequence = []
+let playersequence = []
+let gameover = 0
+let level = 1
+let click = 0
+let notification = document.getElementById("notification")
+//Check Button Clicked Sequence
+function checksequence() {
+    let equal = 1
+    for (let i = 0; i < simonsequence.length; i++) {
+        if (simonsequence[i] != playersequence[i]) {
+            equal = 0
+            break;
+        }
+    }
+    if (equal) {
+        level = level + 1
+    }
+    else gameover = 1
+}
+
+//Backgorund Changing and Updating playerSequence on click
+let buttons = document.querySelectorAll(".btn")
+
+function blink(button) {
+    button.addEventListener('click', () => {
+        let originalColor = button.style.backgroundColor
+        button.style.backgroundColor = "white"
+
+        click += 1
+        setTimeout(() => {
+            button.style.backgroundColor = originalColor
+        }, 200);
+
+
+
+        //Adding clicked button to playersequence
+
+        setTimeout(() => {
+
+            playersequence.push(button)
+            if (playersequence[playersequence.length - 1] != simonsequence[playersequence.length - 1]) {
+                notification.innerHTML = "Game Over, Please Refresh"
+                document.body.style.backgroundColor = "red"
+                overaudio.play()
+
+                return
+            }
+
+            if (click == level) {
+                // console.log("main", simonsequence)
+                // console.log("player", playersequence)
+
+                checksequence()
+
+                if (gameover == 1) {
+                    notification.innerHTML = "Game Over, Please Refresh"
+                    document.body.style.backgroundColor = "red"
+                    overaudio.play()
+                }
+                else {
+                    notification.innerHTML = `Level ${level}`
+                    playersequence = []
+                    click = 0
+                    startgame()
+                }
+            }
+        }, 1000);
+
+    }
+    )
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', blink(button))
+});
+
+// Random box
+function randombox() {
+    return Math.floor(Math.random() * 4 + 1)
+}
+
+//White Blink Randomly
+function blinkrandom(button) {
+    let originalColor = button.style.backgroundColor
+    button.style.backgroundColor = "white"
+
+    setTimeout(() => {
+        button.style.backgroundColor = originalColor
+    }, 200);
+}
+
+
+function startgame() {
+    let rand = randombox()
+    let target = document.getElementById(`btn${rand}`)
+    blinkrandom(target)
+    s2.play()
+    simonsequence.push(target)
+}
+//Game Flow
+startgame()
